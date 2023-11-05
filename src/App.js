a0a1ed4ccb5a4125be6b5121870b214d26b9aa44
Fırat Işıldak <firatisildak1@gmail.com>
2023-11-05 10:52:53 +0000

    add timer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import TodoList from './components/TodoList';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -18,21 +18,31 @@ const tema = {
 export const TemaData = React.createContext();//Bu kod, oluşturulan bir TemaVerisi adında bir context'i export anahtar kelimesi ile dışarıya aktarır. Bu, context'i başka dosyalarda da kullanılabilir hale getirir. 
 
 function App() {
-
+  const [timer, setTimer] = useState(0);
   const [colorValue, setDeger] = useState(tema.dark);
-  
-  function ChangeTema(){
-    if(tema.dark===colorValue){
+
+  function ChangeTema() {
+    if (tema.dark === colorValue) {
       setDeger(tema.light);
-    }else{
+    } else {
       setDeger(tema.dark);
     }
   }
 
+  useEffect(() => {
+
+    if (timer !== -1) {
+      const interval = setInterval(() => {
+        setTimer(timer + 1);
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+  }, [timer])
   return (
     <div className="App">
       <button className="buttonColor" onClick={ChangeTema}>
-      <i class="fa-regular fa-moon"></i> {tema.dark===colorValue ? "light mood" : "dark mood"}
+        {tema.dark === colorValue ? <><i className="fa-solid fa-sun"></i>  light mood</> : <><i className="fa-regular fa-moon"></i>  dark mood</>}
+        <div style={{ height: "145px", width: "145px", backgroundColor: "blue", borderRadius: "50%", color: "white", fontSize: "60px", margin: "10px auto", lineHeight: "145px" }}>{timer}</div>
       </button>
       <TemaData.Provider value={colorValue}>
         <TodoList />
